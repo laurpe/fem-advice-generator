21c0b8492985c0f34b3413a7c122775aff8a8526
laurpe <laura.perttila@edu.bc.fi>
2022-03-29 18:53:29 +0300

    desktop version done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import AdviceContainer from "./components/AdviceContainer";
 
-function App() {
-    return <div>hello world</div>;
-}
+const App = () => {
+    const [advice, setAdvice] = useState("");
+
+    const handleClick = async () => {
+        const advice = await fetch("https://api.adviceslip.com/advice")
+            .then((response) => response.json())
+            .then((data) => data.slip.advice);
+        setAdvice(advice);
+    };
+
+    return <AdviceContainer advice={advice} handleClick={handleClick} />;
+};
 
 export default App;
